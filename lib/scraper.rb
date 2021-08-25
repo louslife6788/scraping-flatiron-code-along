4ba3b0d45e9030ceccf99b8a4878d6008f0e01c1
luis leon <leonluis380@yahoo.com>
2021-08-24 20:14:22 -0400

    commit tree 58667b1afab78c6a2fcb32b0155e3ba9f9d6154c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,25 +11,9 @@ class Scraper
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc  = Nokogiri::HTML(html)
     
-    # puts x.first.css(".date").text
-    # puts x.first.css("h2").text
-    # puts x.first.css("p").text
-    # binding.pry
   end
 
-  # def get_page
-    
-  #   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-  #   doc.css(".post").each do |post|
-  #     course = Course.new
-  #     course.title = post.css("h2").text
-  #     course.schedule = post.css(".date").text
-  #     course.description = post.css("p").text
-  #     # binding.pry
-  #   end
-
-  # end
+ 
 
   def get_courses
     self.get_page.css(".post")
@@ -66,9 +50,7 @@ class Scraper
 end
 
 
- lou = Scraper.new
- lou.print_courses
- Course.all
+ 
 
 
 
